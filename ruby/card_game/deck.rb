9350fb58f9b8f064fdbc55e9f99209158825bfc4
michaelhatch <mike.hatch@tastytrade.com>
2017-10-14 21:28:03 -0500

    moved deck/deck_test. added take, empty, +

---
+++ ruby/card_game/deck.rb
@@ -0,0 +1,33 @@
+require './card'
+
+class Deck
+    attr_accessor :cards
+    
+    def initialize
+      @cards = []
+      Card::SUITS.each do |suit|
+        Card::NAME_VALUES.keys.each do |name|
+          @cards << Card.new(suit, name)
+        end
+      end
+    end
+  
+    def shuffle
+      @cards.shuffle!
+    end
+
+    def take(count = 1)
+      @cards.pop(count)
+    end
+
+    def empty?
+      @cards.empty?
+    end
+
+    def +(other)
+      combined = Deck.new
+      combined.cards.clear
+      combined.cards = self.cards + other.cards
+      combined
+    end
+  end
---
+++ ruby/card_game/deck_test.rb
@@ -0,0 +1,56 @@
+require_relative './deck'
+require 'minitest/autorun'
+
+class DeckTest < Minitest::Test
+    def setup
+      @deck = Deck.new
+    end
+    
+    def test_new_deck_has_52_cards
+      assert_equal @deck.cards.size, 52
+    end
+
+    def test_shuffled_deck_has_52_cards
+      @deck.shuffle
+      assert_equal @deck.cards.size, 52
+    end
+    
+    def test_taken_card_does_not_remain_in_deck
+      card = @deck.take
+      refute @deck.cards.include?(card)
+    end
+
+    def test_taken_card_reduces_deck_size_by_taken_count
+      @deck.take(10)
+      assert_equal @deck.cards.size, 42
+    end
+
+    def test_take_defaults_to_single_card_taken
+      @deck.take
+      assert_equal @deck.cards.size, 51
+    end
+
+    def test_empty_true_for_empty_deck
+      @deck.take(52)
+      assert @deck.empty?
+    end
+
+    def test_empty_false_for_non_empty_deck
+      refute @deck.empty?
+    end
+
+    def test_adding_another_deck_combines_all_cards
+      second_deck = Deck.new
+      combined_deck = @deck + second_deck
+
+      assert_equal 104, combined_deck.cards.size
+    end
+
+    def test_adding_another_deck_leaves_original_decks_unchanged
+      second_deck = Deck.new
+      @deck + second_deck
+
+      assert_equal 52, @deck.cards.size
+      assert_equal 52, second_deck.cards.size
+    end
+  end
